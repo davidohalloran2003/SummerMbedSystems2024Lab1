ff6eafd64901d939a6dd8420966d0dfc3a370b28
davidohalloran2003 <davidohalloran2003@gmail.com>
2024-06-17 10:13:18 +0000

    Rework Changes 1 - Lab1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,36 @@
 #include "mbed.h"
-#include <cstdio>
+
+
 /* 
+
 a few words max 100 on the program.
+For InterruptIn bluebtn, Right click BUTTON1 in brackets and >Go To Declaration  which will give the definition of location of #define BUTTON1  PC_13  // BUTTON_EXTI13 [B2] - which also again if right click on PC_13
+>Go To Declaration which will give the definition of location of PC_13      = 0x2D
+Right Click and >Go to Declaration will give further info on your code
+Original Error "Error Message: Mutex: 0x200015FC, Not allowed in ISR context" - Fix: Disable and enable irq 
+
 */
 
 InterruptIn bluebtn(PC_13);
-volatile int int_trig = 0;
 
-void bttnFall(){
-    int_trig = 1;
+void bttnFall () {
+printf("bttnfall triggerd");       
 }
 
+void bttnRise() {
+    printf("bttnRise triggerd");
+}
 
 // main() runs in its own thread in the OS
 int main()
 {
+        bluebtn.fall(&bttnFall);
+        bluebtn.rise(&bttnRise);
 
-bluebtn.fall(&bttnFall);
+//This program is for the Lab 1 embedded systems module 
 
-    // this is a program for lab 1 embedded systems
+while (true) {
 
-    while (true) {
-        if(int_trig == 1){
-            int_trig = 0;
-            printf("bttnFall \r\n");
-
-        
-           
-
-        }
 }
+
 }
